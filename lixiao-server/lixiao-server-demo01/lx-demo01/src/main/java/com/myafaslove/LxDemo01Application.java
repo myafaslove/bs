a0241291be35d7cd8cc3e1c8aca2a7f858dec9b4
myafaslove <771102443@qq.com>
2023-02-18 23:07:39 +0800

    lx-server-demo01

---
+++ lixiao-server/lixiao-server-demo01/lx-demo01/src/main/java/com/myafaslove/LxDemo01Application.java
@@ -0,0 +1,13 @@
+package com.myafaslove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LxDemo01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LxDemo01Application.class, args);
+    }
+
+}
---
+++ lixiao-server/lixiao-server-demo01/lx-demo01/src/test/java/com/myafaslove/LxDemo01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myafaslove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LxDemo01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
